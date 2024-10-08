4935449ae6ebcb63434f9a14557951e5dcfff811
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-10-08 22:21:42 +0530

    moved older version folder archive

---
+++ extension/archive/background.js
@@ -0,0 +1,37 @@
+let userInfo = null;
+
+chrome.runtime.onInstalled.addListener(() => {
+  checkAuthStatus();
+});
+
+chrome.cookies.onChanged.addListener((changeInfo) => {
+  if (changeInfo.cookie.domain === 'localhost' && changeInfo.cookie.name === 'auth') {
+    checkAuthStatus();
+  }
+});
+
+function checkAuthStatus() {
+  fetch('https://trackjobs.online/api/check-auth', {
+    method: 'GET',
+    credentials: 'include'
+  })
+  .then(response => response.json())
+  .then(data => {
+    userInfo = data.isAuthenticated ? { isAuthenticated: true } : null;
+    chrome.storage.local.set({ userInfo });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    userInfo = null;
+    chrome.storage.local.set({ userInfo });
+  });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'checkAuth') {
+    chrome.storage.local.get('userInfo', (result) => {
+      sendResponse(result.userInfo);
+    });
+    return true;
+  }
+});
