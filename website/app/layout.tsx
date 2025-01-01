03d49799bb238b74ca1e0ce3c983e35dfb1bb39d
techlism <2021itb049.kundan@students.iiests.ac.in>
2025-01-01 17:46:28 +0530

    adding a different color scheme

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,48 +1,49 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "TrackJobs",
-  description: "Track your job applications effortlessly within seconds.",
-  creator: "Techlism",
-  keywords: ['jobs', 'application tracker'],
-  openGraph: {
-    images: 'https://trackjobs.online/trackjobs_og.png',
-    siteName: 'TrackJobs'
-  },
-  twitter: {
-    images: 'https://trackjobs.online/trackjobs_og.png',
-    site: '@trackjobs'
-  }
+	title: "TrackJobs",
+	description: "Track your job applications effortlessly within seconds.",
+	creator: "Techlism",
+	keywords: ["jobs", "application tracker"],
+	openGraph: {
+		images: "https://trackjobs.online/trackjobs_og.png",
+		siteName: "TrackJobs",
+	},
+	twitter: {
+		images: "https://trackjobs.online/trackjobs_og.png",
+		site: "@trackjobs",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className={`${GeistSans.className} flex flex-col min-h-dvh w-full mx-auto `}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <Navbar className="flex-shrink-0" />
-          <main className="flex-grow flex flex-col max-w-7xl mx-auto my-auto items-center justify-center">
-            {children}
-          </main>
-          <Toaster />
-          <Footer className="flex-shrink-0" />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</head>
+			<body
+				className={`${GeistSans.className} flex flex-col min-h-dvh w-full mx-auto `}
+			>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+					<Navbar className="flex-shrink-0" />
+					<main className="flex-grow flex flex-col max-w-7xl mx-auto my-auto items-center justify-center">
+						{children}
+					</main>
+					<Toaster />
+					<Footer className="flex-shrink-0" />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
